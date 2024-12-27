27dddf2b9f3459c59a897a38f0d4bff73dcbfa46
Dmitry Borisov <dmitry.borisov@hazelops.com>
2024-12-28 00:57:12 +0300

    Variable type added

--- variables.tf
+++ variables.tf
@@ -37,6 +37,7 @@ variable "force_delete" {
 }
 
 variable "image_tag_mutability" {
+  type        = string
   default     = "MUTABLE"
   description = "The tag mutability setting for the repository. Must be one of: `MUTABLE` or `IMMUTABLE`. Defaults to `MUTABLE`."
 }
